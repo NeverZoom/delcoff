b2bfb329f476ce1e4696ab03fcafcd84bf67ff6e
Кирилл Панченко <mrneverzoom@gmail.com>
2023-05-02 13:42:37 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,101 @@
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+
+import 'magnific-popup/dist/jquery.magnific-popup.min.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	$('.nav-slider .item').on('click', function(){
+		var slide = $(this).attr('data-nav');
+		$('.nav-slider .item').removeClass('active');
+		$(this).addClass('active');
+		$('.data-slide').removeClass('active');
+		$('.catalog-products').removeClass('active');
+		$('*[data-slide='+ slide +']').addClass('active');
+	});
+
+	$('.catalog-products .type-item').on('click', function(){
+		var slide = $(this).attr('data-type');
+
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+
+		$(this).parent().siblings('.data-type-select').removeClass('active');
+		$(this).parent().siblings('*[data-type-select='+ slide +']').addClass('active');
+
+	});
+
+	$('.service_popup').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		showCloseBtn: false,
+		focus: '#name',
+		mainClass: 'mfp-with-zoom',
+		zoom: {
+			enabled: true,
+			duration: 300,
+			easing: 'ease-in-out',
+			opener: function(openerElement) {
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		},
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+	if (document.querySelector('.map')) {
+		;(function() {
+			if (typeof ymaps === 'undefined') {
+				return;
+			}
+		
+			ymaps.ready(function () {
+				var ymap = document.querySelector('.map');
+  	    var coordinates = ymap.getAttribute('data-coordinates');
+  	    var address = ymap.getAttribute('data-address');
+				var myMap = new ymaps.Map('ymap2', {
+								center: coordinates.split(','),
+								zoom: 17
+						}, {
+								searchControlProvider: 'yandex#search'
+						}),
+						myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+								balloonContent: address
+						});
+				myMap.geoObjects.add(myPlacemark);
+				myMap.behaviors.disable('scrollZoom');
+		});
+		})();
+	}
+
+	$('.burger').on('click', function() {
+		$('.mobile-menu').addClass('active');
+	});
+
+	$('.mobile-menu .close').on('click', function() {
+		$('.mobile-menu').removeClass('active');
+	});
+
+	$('.about-info .border-block .button').on('click', function() {
+		$(this).siblings('span').removeClass('d-none');
+		$(this).remove();
+	});
+
+	if(document.documentElement.clientWidth < 992) {
+		$('.catalog-products .type-item').removeClass('active');
+		$('.catalog-products .type-item').on('click', function() {
+			$(this).siblings('.catalog-column').hide( "fast");
+			$(this).next('.catalog-column').show( "fast");	
+		});
+	}
+
+})
